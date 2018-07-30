7ab02cc388fc5aadd8f1c86fc7c4242b1f01c717
Josef <josefwijaya@outlook.com>
2018-07-30 15:29:13 +0800

    update gkutil test - minor update

--- test/utils/gkUtil.test.js
+++ test/utils/gkUtil.test.js
@@ -2,20 +2,20 @@
 
 var Web4 = typeof window !== 'undefined' ? window.Web4 : require('../../index');
 var expect = typeof window !== 'undefined' ? window.expect : require('chai').expect;
-var globalOrWindow = (typeof window !== 'undefined' ? window : global);
+var glOrWd = (typeof window !== 'undefined' ? window : global);
 
 describe('web4.utils.gkUtil', function () {
-  const web4 = new Web4({ url: 'http://54.95.41.253:1337/rpc', protocol: 'jsonrpc' });
+  const web4 = new Web4({ url: glOrWd.tnet, protocol: 'jsonrpc' });
 
-  it('should have util object', function () {
+  it('should have "util" object', function () {
     expect(web4.utils.gkUtil.util).to.be.an('object');
   });
 
-  it('should have crypto object', function () {
+  it('should have "crypto" object', function () {
     expect(web4.utils.gkUtil.crypto).to.be.an('object');
   });
 
-  it('should have util object', function () {
+  it('should have "mnemonic" object', function () {
     expect(web4.utils.gkUtil.mnemonic).to.be.an('object');
   });
 });
