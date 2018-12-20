743115ce493d9338a949bc4704352893be729cf4
Josef <josefwijaya@outlook.com>
2018-12-20 11:16:22 +0800

    update contract

--- test/intergallactic/contract.td.js
+++ test/intergallactic/contract.td.js
@@ -6,52 +6,149 @@ var BigNumber = typeof window !== 'undefined' ? window.BigNumber : require('bign
 var errorMessage;
 var _contractTd = {};
 
-_contractTd = {
-  instantiate: {
-    valid: [
-      // should create simple contract with one method from abi with implicit type name
-      {
-        input: {
-          abi: [{ name: 'test', type: 'function', inputs: [{ name: 'a', type: 'uint256' }], outputs: [{ name: 'd', type: 'uint256' }] }]
-        },
-        validate: (output) => {
-          expect(output.test).to.be.a('function');
+const testAcc = {
+  address: 'acFVrNat8Y8Evid4fcJzN5KxyEAyuHS6Tuu',
+  privKey: 'ski47BSAmY6PJ9KMHXHMzk7tG8nXTJaKKF2BTRPzmjJ3NAzy1HxMAz336JiN7N8KzF786T2mptHHbBY5fmFeoaNukokkF66',
+  pubKey: 'pkCogxsiXdTj9yn62cXN6L5NHwcrBfS8N2bYhob4HTPDExJfWpD'
+}
+
+_contractTd.instantiate = {
+  valid: [
+    // should create simple contract with one method from abi with implicit type name
+    {
+      input: {
+        contract: {
+          // eslint-disable-next-line
+          abi: [{"constant":true,"inputs":[],"name":"getNumber","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"initial","type":"int256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}],
+          address: '0x'
         }
       },
-      // should create contract with multiple methods
-      {
-        input: {
-          abi: [{ name: 'test', type: 'function', inputs: [{ name: 'a', type: 'uint256' }], outputs: [{ name: 'd', type: 'uint256' }] }, { name: 'test2', type: 'function', inputs: [{ name: 'a', type: 'uint256' }], outputs: [{ name: 'd', type: 'uint256' }] }]
-        },
-        validate: (output) => {
-          expect(output.test).to.be.a('function');
-          expect(output.test2).to.be.a('function');
-        }
-      },
-      {
-        input: {
-          abi: [{ name: 'test', inputs: [{ name: 'a', type: 'uint256' }], outputs: [{ name: 'd', type: 'uint256' }] }]
-        },
-        validate: (output) => {
-          expect(output.test).to.be.a('undefined');
-        }
-      },
-      {
-        input: {
-          abi: [{"constant":true,"inputs":[{"name":"","type":"bytes"},{"name":"","type":"bool"},{"name":"","type":"uint256[]"}],"name":"sam","outputs":[],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"x","type":"uint32"},{"name":"y","type":"bool"}],"name":"baz","outputs":[{"name":"r","type":"bool"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes3[2]"}],"name":"bar","outputs":[],"payable":false,"stateMutability":"pure","type":"function"}]
-        },
-        validate: (output) => {
-          expect(output.sam).to.be.a('function');
-          expect(output.sam('dave', true, [1, 2, 3]).encodeABI()).to.equal('a5643bf20000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000000464617665000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000003000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000003');
-          expect(output.baz).to.be.a('function');
-          expect(output.baz(69, true).encodeABI()).to.equal('cdcd77c000000000000000000000000000000000000000000000000000000000000000450000000000000000000000000000000000000000000000000000000000000001');
-          expect(output.bar).to.be.a('function');
-          expect(output.bar(['abc', 'def']).encodeABI()).to.equal('fce353f661626300000000000000000000000000000000000000000000000000000000006465660000000000000000000000000000000000000000000000000000000000');
-        }
+      validate: (output) => {
+        expect(output.getNumber).to.be.a('function');
       }
-    ]
-  }
+    },
+    // should create contract with multiple methods
+    {
+      input: {
+        contract: {
+          // eslint-disable-next-line
+          abi: [{"constant":false,"inputs":[{"name":"n","type":"int256"}],"name":"setNumber","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getNumber","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"initial","type":"int256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}],
+          address: '0x'
+        }
+      },
+      validate: (output) => {
+        expect(output.getNumber).to.be.a('function');
+        expect(output.setNumber).to.be.a('function');
+      }
+    },
+    {
+      input: {
+        contract: {
+          // eslint-disable-next-line
+          abi: [{"constant":true,"inputs":[{"name":"","type":"bytes"},{"name":"","type":"bool"},{"name":"","type":"uint256[]"}],"name":"sam","outputs":[],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"x","type":"uint32"},{"name":"y","type":"bool"}],"name":"baz","outputs":[{"name":"r","type":"bool"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes3[2]"}],"name":"bar","outputs":[],"payable":false,"stateMutability":"pure","type":"function"}],
+          address: '0x'
+        }
+      },
+      validate: (output) => {
+        expect(output.sam).to.be.a('function');
+        // expect(output.sam('dave', true, [1, 2, 3]).encodeABI()).to.equal('a5643bf20000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000000464617665000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000003000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000003');
+        expect(output.baz).to.be.a('function');
+        expect(output.bar).to.be.a('function');
+      }
+    }
+  ]
 };
+
+_contractTd.deploy = {
+  valid: [
+    // should be able to deploy simple contract
+    {
+      input: {
+        privKey: testAcc.privKey,
+        contract: {
+          // eslint-disable-next-line
+          abi: [{"constant":false,"inputs":[{"name":"n","type":"int256"}],"name":"setNumber","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getNumber","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"initial","type":"int256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}],
+          byteCode: '608060405234801561001057600080fd5b5060405160208061010d8339810180604052602081101561003057600080fd5b505160005560ca806100436000396000f3fe6080604052600436106042577c010000000000000000000000000000000000000000000000000000000060003504639cf27f3781146047578063f2c9ecd814606f575b600080fd5b348015605257600080fd5b50606d60048036036020811015606757600080fd5b50356093565b005b348015607a57600080fd5b5060816098565b60408051918252519081900360200190f35b600055565b6000549056fea165627a7a723058202650623bb64748254160f7b6dc0bb51ae0062aa9a33c575889ade0058d12ca0c0029',
+          gasLimit: 21000,
+          payableAmt: 0
+        },
+        args: [1, 2]
+        // code: 'pragma solidity ^0.4.16; contract Foo { function bar(bytes3[2] memory) public pure {} function baz(uint32 x, bool y) public pure returns (bool r) { r = x > 32 || y; } function sam(bytes memory, bool, uint[] memory) public pure {} }'
+        // code: 'pragma solidity ^0.5.1;contract test { int number; constructor (int initial) public { number = initial; } function getNumber () public view returns (int) { return number; } function setNumber (int n) public { number = n; } }'
+      },
+      validate: (output) => {
+
+      }
+    }
+  ]
+};
+
+_contractTd.methods = {
+  valid: [
+    // should be able to call contract method
+    {
+      input: {
+        privKey: testAcc.privKey,
+        contract: {
+          // eslint-disable-next-line
+          abi: [{"constant":false,"inputs":[{"name":"n","type":"int256"}],"name":"setNumber","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getNumber","outputs":[{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"initial","type":"int256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}],
+          address: '0xca35b7d915458ef540ade6068dfe2f44e8fa733c'
+        },
+        methodName: 'setNumber',
+        methodValue: [10]
+      },
+      validate: (output) => {
+        expect(output).to.eql('9cf27f37000000000000000000000000000000000000000000000000000000000000000a')
+      }
+    },
+    {
+      input: {
+        privKey: testAcc.privKey,
+        contract: {
+          // eslint-disable-next-line
+          abi: [{"constant":true,"inputs":[{"name":"","type":"bytes"},{"name":"","type":"bool"},{"name":"","type":"uint256[]"}],"name":"sam","outputs":[],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"x","type":"uint32"},{"name":"y","type":"bool"}],"name":"baz","outputs":[{"name":"r","type":"bool"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes3[2]"}],"name":"bar","outputs":[],"payable":false,"stateMutability":"pure","type":"function"}],
+          address: '0x'
+        },
+        methodName: 'sam',
+        methodValue: ['dave', true, [1, 2, 3]]
+      },
+      validate: (output) => {
+        expect(output).to.equal('a5643bf20000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000000464617665000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000003000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000003');
+      }
+    },
+    {
+      input: {
+        privKey: testAcc.privKey,
+        contract: {
+          // eslint-disable-next-line
+          abi: [{"constant":true,"inputs":[{"name":"","type":"bytes"},{"name":"","type":"bool"},{"name":"","type":"uint256[]"}],"name":"sam","outputs":[],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"x","type":"uint32"},{"name":"y","type":"bool"}],"name":"baz","outputs":[{"name":"r","type":"bool"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes3[2]"}],"name":"bar","outputs":[],"payable":false,"stateMutability":"pure","type":"function"}],
+          address: '0x'
+        },
+        methodName: 'baz',
+        methodValue: [69, true]
+      },
+      validate: (output) => {
+        expect(output).to.equal('cdcd77c000000000000000000000000000000000000000000000000000000000000000450000000000000000000000000000000000000000000000000000000000000001');
+        // expect(output.baz(69, true).encodeABI()).to.equal('cdcd77c000000000000000000000000000000000000000000000000000000000000000450000000000000000000000000000000000000000000000000000000000000001');
+      }
+    },
+    {
+      input: {
+        privKey: testAcc.privKey,
+        contract: {
+          // eslint-disable-next-line
+          abi: [{"constant":true,"inputs":[{"name":"","type":"bytes"},{"name":"","type":"bool"},{"name":"","type":"uint256[]"}],"name":"sam","outputs":[],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"x","type":"uint32"},{"name":"y","type":"bool"}],"name":"baz","outputs":[{"name":"r","type":"bool"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes3[2]"}],"name":"bar","outputs":[],"payable":false,"stateMutability":"pure","type":"function"}],
+          address: '0x'
+        },
+        methodName: 'bar',
+        methodValue: [['abc', 'def']]
+      },
+      validate: (output) => {
+        expect(output).to.equal('fce353f661626300000000000000000000000000000000000000000000000000000000006465660000000000000000000000000000000000000000000000000000000000');
+      }
+    }
+  ]
+}
 
 if (typeof window !== 'undefined') {
   window._contractTd = _contractTd;
--- test/intergallactic/contract.test.js
+++ test/intergallactic/contract.test.js
@@ -5,27 +5,90 @@ var expect = typeof window !== 'undefined' ? window.expect : require('chai').exp
 var glOrWd = (typeof window !== 'undefined' ? window : global);
 let contractTd = (typeof window !== 'undefined' ? window : require('./contract.td'))._contractTd;
 
-describe('igc.Contract', function () {
+describe.only('igc.Contract', function () {
   const igc = new Intergallactic({ url: glOrWd.tnet, protocol: 'jsonrpc' });
   const Contract = igc.Contract;
-  // const ERC20_ABI = [{'constant': true, 'inputs': [{'name': '_owner', 'type': 'address'}], 'name': 'balanceOf', 'outputs': [{'name': 'balance', 'type': 'uint256'}], 'payable': false, 'type': 'function'}]
-  // const myContract = new Contract(ERC20_ABI);
 
-  // it('should have "deploy" function', () => expect(typeof myContract.deploy === 'function').to.equal(true));
-
-  it('should be able to "instantiate" new contract given abi array', function (done) {
+  it('should be able to "instantiate" contract', function (done) {
     const test = {
       function: (input) => {
-        const myContract = new Contract(input.abi);
-
+        const myContract = new Contract(input.contract);
         return myContract;
       },
-      validate: (output) => {
-        expect(output instanceof Error).to.equal(false);
+      validate: (res) => {
+        expect(res instanceof Error).to.equal(false);
       }
     }
 
     test.data = contractTd.instantiate.valid;
     glOrWd.runTest(test, done);
+  });
+
+  it.skip('should be able to "deploy" new contract', function (done) {
+    const test = {
+      function: (input) => {
+        const myContract = new Contract(input.contract);
+        return myContract.deploy(input.privKey, input.deploy);
+      },
+      validate: (res) => {
+        // expect(res instanceof Error).to.equal(false);
+        // expect(res.statusCode).to.equal(200);
+        // expect(res.body.error).to.equal(undefined);
+        // expect(res.body.result).to.be.an('object');
+        // expect(res.body.result.TxHash).to.be.a('string');
+      }
+    };
+    test.data = contractTd.deploy.valid;
+
+    glOrWd.runTest(test, done);
+  });
+
+  it('should be able to execute contract "encodeABI" after instantiate contract with address', function (done) {
+    const test = {
+      function: (input) => {
+        const myContract = new Contract(input.contract);
+        // return myContract[input.methodName](input.methodValue).encodeABI();
+        // return (myContract[input.methodName].call(null, input.methodValue)).encodeABI();
+        return myContract[input.methodName].apply(this, input.methodValue).encodeABI();
+      },
+      validate: (res) => {
+        expect(res instanceof Error).to.equal(false);
+      }
+    }
+
+    test.data = contractTd.methods.valid;
+    glOrWd.runTest(test, done);
+  });
+
+  it('should be able to execute contract "call" after instantiate contract with address', function (done) {
+    const test = {
+      function: (input) => {
+        const myContract = new Contract(input.contract);
+        return myContract[input.methodName].apply(this, input.methodValue).call();
+      },
+      validate: (res) => {
+        expect(res instanceof Error).to.equal(false);
+      }
+    }
+
+    test.data = contractTd.methods.valid;
+    glOrWd.runTest(test, done);
+  });
+
+  it.skip('should be able to call contract "ABI Methods" after deployment', function (done) {
+    throw new Error('Implementation')
+    // const test = {
+    //   function: (input) => {
+    //     const myContract = new Contract(input);
+
+    //     return myContract;
+    //   },
+    //   validate: (res) => {
+    //     expect(res instanceof Error).to.equal(false);
+    //   }
+    // }
+
+    // test.data = contractTd.instantiate.valid;
+    // glOrWd.runTest(test, done);
   });
 });
